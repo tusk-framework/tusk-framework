a5a6c802026065d38ef36e2a5366d0d3a1997558
Jackson Sá <jacksonwendel@gmail.com>
2026-06-01 22:49:12 -0300

    fix: make Route attribute repeatable

--- tusk-web/src/Attribute/Route.php
+++ tusk-web/src/Attribute/Route.php
@@ -4,7 +4,7 @@ namespace Tusk\Web\Attribute;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_METHOD)]
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class Route
 {
     public function __construct(
